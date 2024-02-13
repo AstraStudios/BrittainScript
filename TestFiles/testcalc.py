0b81e778ff60211d431c459647f277c8dec84ddb
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2024-02-12 22:39:18 -0600

    Testing language complete and working

--- TestFiles/testcalc.py
+++ TestFiles/testcalc.py
@@ -4,15 +4,16 @@ import testparser
 def parse_input(input_text):
     #input_string = ' '.join(str(tok.value) for tok in input_text)
     #testlexer.lexer.input(input_text)
+    testlexer.lexer.input(input_text)
     tokens = []
     while True:
         tok = testlexer.lexer.token()
         if not tok:
             break
+        print(tok)
         tokens.append(tok)
-    input_string = ' '.join(str(tok.value) for tok in input_text)
     #result = testparser.parse(tokens)
-    result = testparser.parse(input_string)
+    result = testparser.parse(input_text)
     return result
 
 input_text = input("Test: ")
