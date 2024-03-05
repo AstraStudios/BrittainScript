2c22da7cfe6a521ee52d6fa240475dd89d13c8a4
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2024-03-04 23:22:37 -0600

    Begin final product

---
+++ BrittainScriptInternals/main.py
@@ -0,0 +1,21 @@
+from mathlexer import *
+from textlexer import *
+from parser import *
+
+#def detect_characters(data):
+    #math_operators = {'+', '-','*','/'} # add more when i add more
+    #return any(char in math_operators for char in data)
+
+def parse_file(file_path):
+    with open(file_path, 'r') as file:
+        data = file.read
+        math_data = []
+        text_data = []
+        
+        for line in file:
+            if any(char in line for char in {'+','-','*','/'}):
+                math_data.append(line)
+            else:
+                text_data.append(line)
+        
+        #math_lexer = 
